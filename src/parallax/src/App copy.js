12ffb4c9a8a7c935808eb4310e84dc8b10811453
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-03-17 11:30:16 -0500

    Fixed bug in parallax/App.js

--- src/parallax/src/App copy.js
+++ src/parallax/src/App copy.js
@@ -26,7 +26,7 @@ function App() {
   
   </div>
   {/* First Parallax Image with Logo Text */}
-  <div className="bgimg-1 w3-display-container w3-opacity-min"   style={{height:"100vh"}} id="home">
+  <div className="bgimg-1 w3-display-container w3-opacity-min" id="home">
     <div className="w3-display-middle" style={{ whiteSpace: "nowrap" }}>
       <span className="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">
         MY <span className="w3-hide-small">WEBSITE</span> LOGO
