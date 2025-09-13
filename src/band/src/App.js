815d8c254dfd4fe50f6458c8ba25dd75ba197997
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-09-12 21:13:33 -0500

    Implemented carousel with useEffect

--- src/band/src/App.js
+++ src/band/src/App.js
@@ -1,5 +1,34 @@
 import { myFunction } from "../src/utils/myFunction"
+import { useEffect } from "react";
+
+var myIndex = 0;
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000);    
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 function App() {
+
+  useEffect(()=>{
+carousel();
+
+  })
 
   return (
 <>
